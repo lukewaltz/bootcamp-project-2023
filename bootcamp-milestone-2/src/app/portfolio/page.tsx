caa9feead33f0b69b918cf62a91c1d592afb2c2b
Luke Waltz <lukewaltz@outlook.com>
2024-01-04 17:23:22 -0800

    feat: fixed map keys in portfolio page

--- bootcamp-milestone-2/src/app/portfolio/page.tsx
+++ bootcamp-milestone-2/src/app/portfolio/page.tsx
@@ -1,47 +1,51 @@
 import React from "react";
-import style from "./portfolio.module.css"
+import style from "./portfolio.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import Project from '@/database/projectSchema';
-import connectDB from '@/helpers/db';
+import Project from "@/database/projectSchema";
+import connectDB from "@/helpers/db";
 import ProjectComponent from "@/components/projectComponent";
 
-async function getProjects(){
-	await connectDB() // function from db.ts before
+async function getProjects() {
+  await connectDB(); // function from db.ts before
 
-	try {
-			// query for all blogs and sort by date
-	    const projects = await Project.find().lean().orFail()
-			// send a response as the blogs as the message
-      console.log(projects)
-	    return projects
-	} catch (err) {
+  try {
+    // query for all blogs and sort by date
+    const projects = await Project.find().lean().orFail();
+    // send a response as the blogs as the message
+    console.log(projects);
+    return projects;
+  } catch (err) {
     console.error("error getting proj data from DB ", err);
-	    return null
-	}
+    return null;
+  }
 }
 
 export default async function Portfolio() {
-
   const projectData = await getProjects();
-  (projectData ? console.log(projectData[0]) : null);
+  projectData ? console.log(projectData[0]) : null;
 
   console.log("project data:", projectData);
 
   return (
     <header className={style.portfolio}>
       <main>
-          {projectData ? projectData.map((project) => (
-             <div key={project._id}>
-               <Link 
-               href={"/portfolio/" + project.slug}
-               >
-                <ProjectComponent {...project}/>
-               </Link>
-
-            </div>
-          )) : null}
-
-    </main>
+        {projectData
+          ? projectData.map((project, index) => (
+              <div key={index}>
+                <Link href={"/portfolio/" + project.slug}>
+                  <ProjectComponent
+                    name={project.name}
+                    slug={project.slug}
+                    date={project.date}
+                    image={project.image}
+                    content={project.content}
+                  />
+                </Link>
+              </div>
+            ))
+          : null}
+      </main>
     </header>
-  );}
+  );
+}
